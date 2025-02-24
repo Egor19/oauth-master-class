3f350f5b7c61f9c98d27dc6fd952622cef9dfb52
Egor19 <egorshatalov2002@mail.ru>
2025-02-24 13:25:52 +0500

    add second button

--- src/index.js
+++ src/index.js
@@ -135,7 +135,7 @@ window.onload = async () => {
 
     const scope = "login:email login:info"; // Разрешения
 
-    const authUrl = `https://oauth.yandex.ru/authorize?response_type=code&client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}`;
+    const authUrl = `https://oauth.yandex.ru/authorize?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}`;
     
     localStorage.setItem("last_auth_url", authUrl); // Сохранение перед редиректом
     console.log(authUrl);
